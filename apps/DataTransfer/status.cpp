f706cc178934bfd050a7f02bbfa11821c82d0f4b
liu_bojun <liu_bojun@sina.com>
2017-11-28 09:18:41 +0800

    增加状态控制自绘控件

---
+++ apps/DataTransfer/status.cpp
@@ -0,0 +1,57 @@
+﻿#include "status.h"
+#include <QPainter>
+
+CStatusLabel::CStatusLabel()
+{
+    m_bIsStart = false;
+    m_bLastGreen = false;
+
+    m_oTimer.setInterval(500);
+    connect(&m_oTimer, SIGNAL(timeout()), this, SLOT(update()));
+}
+
+CStatusLabel::~CStatusLabel()
+{
+
+}
+
+void CStatusLabel::paintEvent(QPaintEvent *event)
+{
+    if (m_bIsStart)
+    {
+        // 通过标识位m_bLastGreen记录上次的状态，两种状态不断切换
+        if (!m_bLastGreen)
+        {
+            QPainter painter(this);
+            painter.setBrush(Qt::green);
+            painter.drawEllipse(QPoint(this->geometry().center().x()-10, this->geometry().center().y()-8), 6, 6);
+            m_bLastGreen = true;
+        }
+        else
+        {
+            m_bLastGreen = false;
+        }
+
+    }
+    else
+    {
+        m_bLastGreen = false;
+    }
+
+}
+
+void CStatusLabel::start()
+{
+    m_bIsStart = true;
+    m_bLastGreen = false;
+    // 启动定时器
+    m_oTimer.start();
+}
+
+void CStatusLabel::stop()
+{
+    m_bIsStart = false;
+    //停止定时器
+    m_oTimer.stop();
+}
+
---
+++ apps/DataTransfer/status.h
@@ -0,0 +1,37 @@
+﻿#ifndef status_h__
+#define status_h__
+
+#include <QWidget>
+#include <QTimer>
+
+/************************************************************************/
+/*                  用途：实现工作指示灯闪烁效果控件
+					作者: 刘伯俊
+					日期: 2017-11-28
+/************************************************************************/
+
+class CStatusLabel : public QWidget
+{
+    Q_OBJECT
+public:
+    CStatusLabel();
+
+    virtual ~CStatusLabel();
+
+    // 启动闪烁
+    void start();
+
+    // 停止闪烁
+    void stop();
+
+protected:
+    // 重写paintEvent实现指示灯闪烁效果
+    virtual void paintEvent(QPaintEvent *event);
+
+private:
+    QTimer m_oTimer;;
+    // 标识是否启动闪烁
+    bool m_bIsStart;
+    bool m_bLastGreen;
+};
+#endif // status_h__
